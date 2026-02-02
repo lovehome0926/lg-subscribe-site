1e11e1774f01bafc9625a9337f7e900580e31893
lovehome0926 <dnc.homeapp@gmail.com>
2026-02-02 16:35:13 +0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,21 @@ export default {
       colors: {
         'lg-red': '#e60044',
         'lg-dark': '#05090f',
+        'lg-pearl': '#f8f9fa',
+        'lg-brand-bg': '#f6f3ef',
         'lg-beige': '#e7ded0',
       },
       fontFamily: {
         sans: ['Outfit', 'sans-serif'],
+      },
+      animation: {
+        'scroll': 'scroll 40s linear infinite',
+      },
+      keyframes: {
+        scroll: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-50%)' },
+        }
       }
     },
   },
