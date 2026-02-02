bd002444dbe4cf4e3afa8c7d3f52e4700751f5c1
lovehome0926 <dnc.homeapp@gmail.com>
2026-02-02 14:24:06 +0800

    new

--- index.tsx
+++ index.tsx
@@ -1,14 +1,1 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-
-if (rootElement) {
-  const root = createRoot(rootElement);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
+// Logic moved to index.html for single-file deployment (anti-white-screen)
